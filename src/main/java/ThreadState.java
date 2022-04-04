85bd6424d970579caa45f7b94e486c11d499410c
Oleg <hornets@list.ru>
2022-04-04 21:38:36 +0300

    1.1. Состояние нити. [#229175]

---
+++ src/main/java/ThreadState.java
@@ -0,0 +1,25 @@
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> {}
+        );
+        Thread second = new Thread(
+                () -> {}
+        );
+
+        System.out.printf("%s state: %s, %s state: %s\n",
+                first.getName(), first.getState(),
+                second.getName(), second.getState());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+            System.out.printf("%s state: %s, %s state: %s\n",
+                    first.getName(), first.getState(),
+                    second.getName(), second.getState());
+        }
+        System.out.printf("%s state: %s, %s state: %s\n",
+                first.getName(), first.getState(),
+                second.getName(), second.getState());
+        System.out.println("Работа завершена");
+    }
+}
