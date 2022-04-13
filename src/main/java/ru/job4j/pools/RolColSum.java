8a7e3900e69de06d2bbaa17da1c0d0d779b1ad54
Oleg Sergienko <osesergienko@sberbank.ru>
2022-04-13 13:53:18 +0300

    4. CompletableFuture

---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,73 @@
+package ru.job4j.pools;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class RolColSum {
+    public static class Sums {
+        private int rowSum = 0;
+        private int colSum = 0;
+
+        public Sums(int rowSum, int colSum) {
+            this.rowSum = rowSum;
+            this.colSum = colSum;
+        }
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+    }
+
+    public static Sums[] sum(int[][] matrix) {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < sums.length; i++) {
+            sums[i] = new Sums(0, 0);
+        }
+
+        for (int i = 0; i < matrix.length ; i++) {
+            for (int k = 0; k < matrix.length; k++) {
+                sums[i].setRowSum(sums[i].getRowSum() + matrix[i][k]);
+                sums[i].setColSum(sums[i].getColSum() + matrix[k][i]);
+            }
+        }
+        return sums;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Sums[] sums = new Sums[matrix.length];
+        Map<Integer, CompletableFuture<Sums>> futures = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            futures.put(i, getTask(matrix, i));
+        }
+        for (Integer key : futures.keySet()) {
+            sums[key] = futures.get(key).get();
+        }
+        return sums;
+    }
+
+    public static CompletableFuture<Sums> getTask(int[][] matrix, int index) {
+        return CompletableFuture.supplyAsync(() -> {
+            int colSum = 0;
+            int rowSum = 0;
+            for (int k = 0; k < matrix.length; k++) {
+                rowSum += matrix[index][k];
+                colSum += matrix[k][index];
+            }
+            return new Sums(rowSum, colSum);
+        });
+    }
+}
